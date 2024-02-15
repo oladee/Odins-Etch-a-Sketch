0e3a6e7b3802d54c9d5aa953fdc0b604d9aecca7
Oladee <deemajor230600@gmail.com>
2024-02-15 22:08:42 +0100

    finished with the game

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 let frame = document.getElementById('frame')
+let reset = document.querySelector('button')
 
-for (i = 0; i < 16; i++){
-    let div = document.createElement('section')
-    div.setAttribute('id', `section${i}`)
-    frame.appendChild(div)
-    for( j = 0; j < 16; j++){
-        div.innerHTML += '<div class="box"></div>'
+let limit = 16
+
+function heatMa(){
+    let items = [...document.querySelectorAll('.box')]
+    
+    items.forEach((item)=>{
+        
+        item.addEventListener('mouseover',()=>{
+            item.style.backgroundColor = 'red'
+        })
+        item.addEventListener('touchend',()=>{
+            item.style.backgroundColor = 'red'
+        })
+    })
+}
+
+
+reset.addEventListener('click',()=>{
+    let newlimit = prompt('what will be your new dimesion p.s Max of 100')
+    console.log(newlimit)
+    if (newlimit < 101){
+        frame.innerHTML = ""
+        limit = newlimit
+        createBoxes(newlimit);
+    }
+    else{
+        createBoxes(limit)
+    }
+    heatMa()
+})
+
+function createBoxes(limit){
+    for (i = 0; i < limit; i++){
+        let div = document.createElement('section')
+        div.setAttribute('id', `section${i}`)
+        frame.appendChild(div)
+        for( j = 0; j < limit; j++){
+            div.innerHTML += '<div class="box"></div>'
+        }
     }
 }
 
-let items = [...document.querySelectorAll('.box')]
+createBoxes(limit)
+heatMa()
 
-// frame.addEventListener('m')
-
-items.forEach((item)=>{
-    item.addEventListener('mouseover',()=>{
-        item.style.backgroundColor = 'red'
-    })
-    item.addEventListener('touchend',()=>{
-        item.style.backgroundColor = 'red'
-    })
-})
